f94c30a00443a96a6df77ce87f7b963e0281ce4a
Bayram M'KININI <bayram_mkinini.external@discovery.com>
2023-09-22 19:51:42 +0100

    FEAT: Implement navigation

--- app/src/main/java/net/noliaware/yumi/feature_message/domain/model/Message.kt
+++ app/src/main/java/net/noliaware/yumi/feature_message/domain/model/Message.kt
@@ -1,9 +1,11 @@
 package net.noliaware.yumi.feature_message.domain.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 import net.noliaware.yumi.commun.domain.model.Priority
 
+@Keep
 @Parcelize
 data class Message(
     val messageId: String,
